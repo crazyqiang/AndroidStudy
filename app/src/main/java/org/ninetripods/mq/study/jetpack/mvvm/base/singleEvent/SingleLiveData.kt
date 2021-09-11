51caa4b95ed41fc399dadfe43eec2019ce4f53a2
crazyqiang <504091986@qq.com>
2021-09-11 18:00:55 +0800

    [1.4.0-feature]引入singleLiveData 非粘性事件且只有一个Observer能事件通知

---
+++ app/src/main/java/org/ninetripods/mq/study/jetpack/mvvm/base/singleEvent/SingleLiveData.kt
@@ -0,0 +1,46 @@
+package org.ninetripods.mq.study.jetpack.mvvm.base.singleEvent
+
+import android.util.Log
+import androidx.annotation.MainThread
+import androidx.annotation.Nullable
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
+
+/**
+ * 多个观察者存在时，只有一个能够收到数据更新
+ * https://github.com/android/architecture-samples/blob/dev-todo-mvvm-live/todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/SingleLiveEvent.java
+ */
+class SingleLiveData<T> : MutableLiveData<T>() {
+
+    companion object {
+        private const val TAG = "SingleLiveEvent"
+    }
+
+    private val mPending = AtomicBoolean(false)
+
+    @MainThread
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        if (hasActiveObservers()) {
+            Log.w(TAG, "Multiple observers registered but only one will be notified of changes.")
+        }
+
+        // Observe the internal MutableLiveData
+        super.observe(owner) { t ->
+            if (mPending.compareAndSet(true, false)) {
+                observer.onChanged(t)
+            }
+        }
+    }
+
+    override fun setValue(@Nullable value: T?) {
+        mPending.set(true)
+        super.setValue(value)
+    }
+
+    @MainThread
+    fun call() {
+        value = null
+    }
+}
