bb19b88975e724bf0c9ffe8d9f9cbf8b4167cc8c
crazyqiang <504091986@qq.com>
2021-11-03 10:44:55 +0800

    [1.4.0-feature] 更新注释

--- app/src/main/java/org/ninetripods/mq/study/jetpack/mvvm/base/BaseMvvmActivity.kt
+++ app/src/main/java/org/ninetripods/mq/study/jetpack/mvvm/base/BaseMvvmActivity.kt
@@ -27,6 +27,10 @@ abstract class BaseMvvmActivity<VM : BaseViewModel> : BaseActivity() {
     /**
      * 获取ViewModel 子类可以复写，自行初始化
      * 注：初始化ViewModel，还可以通过activity-ktx、fragment-ktx扩展库
+     * 引入方式：
+     * 1、Activity中：implementation "androidx.activity:activity-ktx:1.3.1"
+     * 2、Fragment中：implementation "androidx.fragment:fragment-ktx:1.3.6"
+     * 版本更新参见：https://developer.android.com/kotlin/ktx/extensions-list
      * 初始化方式：val model: VM by viewModels()
      */
     protected open fun getViewModel(): VM? {
