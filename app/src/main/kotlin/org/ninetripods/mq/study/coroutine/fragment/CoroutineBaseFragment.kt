511a6dcb41de716cda49d5cbc3e5f243b47df09a
crazyqiang <504091986@qq.com>
2022-07-27 19:47:20 +0800

    [1.6.0-feature]协程示例

--- app/src/main/kotlin/org/ninetripods/mq/study/coroutine/fragment/CoroutineBaseFragment.kt
+++ app/src/main/kotlin/org/ninetripods/mq/study/coroutine/fragment/CoroutineBaseFragment.kt
@@ -3,10 +3,11 @@ package org.ninetripods.mq.study.coroutine.fragment
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import org.ninetripods.mq.study.R
 import org.ninetripods.mq.study.kotlin.base.BaseFragment
+import kotlin.coroutines.AbstractCoroutineContextElement
+import kotlin.coroutines.CoroutineContext
 
 /**
  * 协程的基本使用
@@ -18,7 +19,12 @@ class CoroutineBaseFragment : BaseFragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        lifecycleScope.launch {
+        val exceptionHandler =
+            CoroutineExceptionHandler { context, throwable -> print("throwable:$throwable") }
+
+        lifecycleScope.launch(
+            context = Dispatchers.Main + Job() + CoroutineName("MyCoroutines") + exceptionHandler,
+            start = CoroutineStart.LAZY) {
             suspendFuc()
         }
     }
@@ -26,5 +32,9 @@ class CoroutineBaseFragment : BaseFragment() {
     private suspend fun suspendFuc(): String {
         delay(1000)
         return "value"
+    }
+
+    class MyElement : AbstractCoroutineContextElement(MyElement) {
+        public companion object Key : CoroutineContext.Key<MyElement>
     }
 }
