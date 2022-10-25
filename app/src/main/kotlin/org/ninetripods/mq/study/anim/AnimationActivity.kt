447fe32268b3247c761c92dcbfacf45ad3d30db3
crazyqiang <504091986@qq.com>
2022-10-26 00:45:48 +0800

    [1.7.0-feature] 补间动画

--- app/src/main/kotlin/org/ninetripods/mq/study/anim/AnimationActivity.kt
+++ app/src/main/kotlin/org/ninetripods/mq/study/anim/AnimationActivity.kt
@@ -1,8 +1,6 @@
 package org.ninetripods.mq.study.anim
 
-import android.view.animation.Animation
-import android.view.animation.LinearInterpolator
-import android.view.animation.RotateAnimation
+import android.view.animation.*
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
@@ -15,6 +13,7 @@ import org.ninetripods.mq.study.kotlin.ktx.id
  * mqcoder90@gmail.com
  */
 class AnimationActivity : BaseActivity() {
+
     private val mTvTarget: ImageView by id(R.id.iv_target)
     private val mBtnRotate: Button by id(R.id.btn_rotate)
     private val mBtnTranslate: Button by id(R.id.btn_translate)
@@ -32,10 +31,10 @@ class AnimationActivity : BaseActivity() {
 
     override fun initEvents() {
         mBtnRotate.setOnClickListener { mTvTarget.startAnimation(loadRotationAnim()) }
-        mBtnScale.setOnClickListener {  }
-        mBtnTranslate.setOnClickListener {  }
-        mBtnAlpha.setOnClickListener {  }
-        mBtnAnimSet.setOnClickListener {  } //组合动画
+        mBtnScale.setOnClickListener { mTvTarget.startAnimation(loadScaleAnimation()) }
+        mBtnTranslate.setOnClickListener { }
+        mBtnAlpha.setOnClickListener { }
+        mBtnAnimSet.setOnClickListener { } //组合动画
         mBtnStop.setOnClickListener { mTvTarget.clearAnimation() }
     }
 
@@ -46,12 +45,12 @@ class AnimationActivity : BaseActivity() {
     private fun loadRotationAnim(): Animation {
         //方式1：代码动态生成
         val rotateAnim: Animation = RotateAnimation(
-            0f,
-            360f,
-            Animation.RELATIVE_TO_SELF,
-            0.5f,
-            Animation.RELATIVE_TO_SELF,
-            0.5f
+                0f,
+                360f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f
         ).apply {
             duration = 2000
             interpolator = LinearInterpolator()
@@ -59,11 +58,26 @@ class AnimationActivity : BaseActivity() {
             repeatCount = Animation.INFINITE //重复次数 INFINITE为无限重复
             startOffset = 0L //动画延迟开始时间 ms
             repeatMode =
-                Animation.RESTART // RESTART：正序重新开始、REVERSE：倒序重新开始，默认是RESTART。注意：repeatCount(count)设置的count值必须>0或者是INFINITE才会生效
+                    Animation.RESTART // RESTART：正序重新开始、REVERSE：倒序重新开始，默认是RESTART。注意：repeatCount(count)设置的count值必须>0或者是INFINITE才会生效
         }
         return rotateAnim
         // 方式2：通过XML创建
-        // return AnimationUtils.loadAnimation(this, R.anim.view_rotate)
+        //return AnimationUtils.loadAnimation(this, R.anim.view_rotate)
+    }
+
+    private fun loadScaleAnimation(): Animation {
+        //方式1：代码动态生成
+        return ScaleAnimation(
+                1.0f, 0.5f, 1.0f, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f).apply {
+            duration = 1000
+            repeatCount = Animation.INFINITE
+            repeatMode = Animation.REVERSE
+            fillAfter = true
+        }
+        // 方式2：通过XML创建
+        //return AnimationUtils.loadAnimation(this, R.anim.view_scale)
     }
 
 
