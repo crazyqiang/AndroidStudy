e3f672e0e93750f4cd202282b185e3a5df04f1e4
crazyqiang <mqcoder90@gmail.com>
2023-06-03 23:45:05 +0800

    [1.7.0-feature] 类委托 & 属性委托

--- app/src/main/kotlin/org/ninetripods/mq/study/function/DelegateCase.kt
+++ app/src/main/kotlin/org/ninetripods/mq/study/function/DelegateCase.kt
@@ -5,19 +5,79 @@ import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
 /**
- * 委托模式 -> 类委托、属性委托
+ * 委托模式 -> 接口类委托、属性委托
  * Created by mq on 2023/5/30
  */
-//class DelegateKt(delegate: ISay) : ISay by delegate
-
 fun main() {
-    val clz = Example()
-    println(clz.str)
-    clz.str = "heheda"
+    /**
+     * 1、类委托
+     */
+    RealImp().sayHello() //方式1
+    RealImp2().sayHello() //方式2
+    RealImp3(DelegateImp()).sayHello() //方式3
+
+    /**
+     * 2、属性委托
+     */
+    val property = DelegateProperty()
+    //p1
+    println(property.p1)
+    property.p1 = "小马快跑"
+    //p2
+    println(property.p2)
+    //p3
+    println(property.p3)
+    property.p3 = "小马快跑"
 }
 
-class Example {
-    var str: String by DelegateRW()
+/**
+ * ------------------------类委托------------------------start
+ */
+interface ISay {
+    fun sayHello()
+}
+
+class DelegateImp : ISay {
+    override fun sayHello() {
+        println("sayHello from DelegateImp")
+    }
+}
+
+class RealImp : ISay {
+    //来自DelegateImp代理对象
+    private val delegate = DelegateImp()
+    override fun sayHello() {
+        delegate.sayHello()
+    }
+}
+
+/**
+ * 上述[RealImp]的实现方式可以直接通过下面的方式来替代
+ */
+class RealImp2 : ISay by DelegateImp() {
+    /**
+     * 如果有需要，[DelegateImp]的sayHello()还可以被复写
+     */
+//    override fun sayHello() {
+//        println("哎呀，我又被复写了！")
+//    }
+}
+
+/**
+ * 上述[RealImp]的实现方式可以直接通过下面的方式来替代
+ */
+class RealImp3(delegate: ISay) : ISay by delegate
+/**
+ * ------------------------类委托------------------------end
+ */
+
+/**
+ * ------------------------属性委托------------------------start
+ */
+class DelegateProperty {
+    var p1: String by Delegate()
+    val p2: String by DelegateR()
+    var p3: String by DelegateRW()
 }
 
 /**
@@ -26,18 +86,20 @@ class Example {
 class Delegate {
     /**
      * 对应属性中的get()，表示获取数据
-     * @param thisRef
-     * @param property
+     * @param thisRef 请求其值的对象，即被委托对象的实例
+     * @param property 被委托对象属性的元数据。包含了被委托对象的属性的名称、类型、可见性等信息。
+     * 通过property对象，我们可以获取被委托对象的属性值，并进行相应的操作。
+     * @return 返回property value
      */
     operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
-        return "$thisRef,${property.name}"
+        return "$thisRef，${property.name}"
     }
 
     /**
      * 对应属性中的set()，表示设置数据，只有var的属性会有
-     * @param thisRef
-     * @param property
-     * @param value
+     * @param thisRef 同上
+     * @param property 同上
+     * @param value 要设置的值
      */
     operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
         println("$thisRef , ${property.name} , $value")
@@ -49,7 +111,7 @@ class Delegate {
  */
 class DelegateR : ReadOnlyProperty<Any, String> {
     override fun getValue(thisRef: Any, property: KProperty<*>): String {
-        return "$thisRef,${property.name}"
+        return "getValue：$thisRef，${property.name}"
     }
 }
 
@@ -58,25 +120,13 @@ class DelegateR : ReadOnlyProperty<Any, String> {
  */
 class DelegateRW : ReadWriteProperty<Any, String> {
     override fun getValue(thisRef: Any, property: KProperty<*>): String {
-        return "$thisRef,${property.name}"
+        return "getValue：$thisRef，${property.name}"
     }
 
     override fun setValue(thisRef: Any, property: KProperty<*>, value: String) {
-        println("$thisRef , ${property.name} , $value")
+        println("setValue：$thisRef，${property.name}，$value")
     }
 }
-
-///**
-// * 类委托
-// */
-//class DelegateImp : ISay {
-//    override fun sayHello() {
-//        println("sayHello from DelegateImp")
-//    }
-//}
-//
-//class RealSubject : ISay by DelegateImp()
-//
-//interface ISay {
-//    fun sayHello()
-//}
+/**
+ * ------------------------属性委托------------------------end
+ */
