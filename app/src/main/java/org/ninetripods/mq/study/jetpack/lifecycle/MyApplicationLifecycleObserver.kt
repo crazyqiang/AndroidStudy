2d1f12cd798148e6227ab94587e19e9ab16701ba
crazyqiang <mqcoder90@gmail.com>
2023-11-09 17:57:15 +0800

    [1.7.0-feature] Lifecycle改用新版本DefaultLifecycleObserver方式

--- app/src/main/java/org/ninetripods/mq/study/jetpack/lifecycle/MyApplicationLifecycleObserver.kt
+++ app/src/main/java/org/ninetripods/mq/study/jetpack/lifecycle/MyApplicationLifecycleObserver.kt
@@ -1,25 +1,22 @@
 package org.ninetripods.mq.study.jetpack.lifecycle
 
 import android.util.Log
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import org.ninetripods.mq.study.jetpack.KConsts
 import org.ninetripods.mq.study.kotlin.ktx.showToast
 
 /**
  * 监听Application的生命周期，可以用来判断应用前后台判断
  */
-class MyApplicationLifecycleObserver : LifecycleObserver {
+class MyApplicationLifecycleObserver : DefaultLifecycleObserver {
 
-    @OnLifecycleEvent(value = Lifecycle.Event.ON_START)
-    fun onAppForeground() {
+    override fun onStart(owner: LifecycleOwner) {
         Log.e(KConsts.LIFE_APPLICATION_TAG, "onAppForeground")
         showToast("应用处于前台")
     }
 
-    @OnLifecycleEvent(value = Lifecycle.Event.ON_STOP)
-    fun onAppBackground() {
+    override fun onStop(owner: LifecycleOwner) {
         Log.e(KConsts.LIFE_APPLICATION_TAG, "onAppBackground")
         showToast("应用处于后台")
     }
