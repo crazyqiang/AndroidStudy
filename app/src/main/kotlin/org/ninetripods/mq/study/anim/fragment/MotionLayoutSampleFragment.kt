87fe90f495eb27eaf4f992420f064175b326ac8d
crazyqiang <mqcoder90@gmail.com>
2024-11-20 18:06:30 +0800

    MotionLayout月亮动画示例

--- app/src/main/kotlin/org/ninetripods/mq/study/anim/fragment/MotionLayoutSampleFragment.kt
+++ app/src/main/kotlin/org/ninetripods/mq/study/anim/fragment/MotionLayoutSampleFragment.kt
@@ -6,6 +6,8 @@ import org.ninetripods.mq.study.kotlin.base.BaseFragment
 class MotionLayoutSampleFragment : BaseFragment() {
 
     override fun getLayoutId(): Int {
+        //layout_circle_menu  放射状态动画
+        //layout_motion_moon 月亮动画
         return R.layout.layout_circle_menu
     }
 }
